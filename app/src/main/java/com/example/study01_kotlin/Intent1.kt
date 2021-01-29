1db3fbfb48948a7ed023865a443b1d617ce00fea
N0mercy00 <dkdhehesp@naver.com>
2021-01-29 23:29:27 +0900

    intent Example done

--- app/src/main/java/com/example/study01_kotlin/Intent1.kt
+++ app/src/main/java/com/example/study01_kotlin/Intent1.kt
@@ -1,6 +1,7 @@
 package com.example.study01_kotlin
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -14,6 +15,7 @@ class Intent1 : AppCompatActivity() {
         var btn_intent1 :TextView = findViewById(R.id.btn_intent1)
 
         btn_intent1.setOnClickListener {
+            //1.인텐트를 이용한 첫번째 방법
 //            var intent = Intent(this@Intent1,Intent2::class.java)
 //            //putExtra를 이용하여 정보를 전송할 수 있다
 //            //putExtra(<이름>,<데이터>)key value 방식으로 전송
@@ -21,16 +23,21 @@ class Intent1 : AppCompatActivity() {
 //            intent.putExtra("number2",2)
 //            startActivity(intent)
 
-            //다른 방법
-            var intent2 = Intent(this@Intent1,Intent2::class.java)
-            //apply 코틀린에서 유용한 기능 대충 apply앞에 붙은애들을 this로 바꾸서 사용 할 수 있게 해줌
-            //가독성이 좋아지니깐 쓰는것 권장
-            intent2.apply {
-                this.putExtra("number1",1)
-                this.putExtra("number2",2)
-            }
-            //요청에 대한 결과값을 받기위해서는 아래 함수를 사용해야함
-            startActivityForResult(intent2,200)
+            //2.다른 방법 <권장방법>
+//            var intent2 = Intent(this@Intent1,Intent2::class.java)
+//            //apply 코틀린에서 유용한 기능 대충 apply앞에 붙은애들을 this로 바꾸서 사용 할 수 있게 해줌
+//            //가독성이 좋아지니깐 쓰는것 권장
+//            intent2.apply {
+//                this.putExtra("number1",1)
+//                this.putExtra("number2",2)
+//            }
+//            //요청에 대한 결과값을 받기위해서는 아래 함수를 사용해야함
+//            startActivityForResult(intent2,200)
+
+            //3.암시적으로 사용하는 방법(가능한 대상에게 요청하는거)
+            //링크열기 누르면 막 여러개 브라우저 뜨는거 있잖아? 선택하라고, 그거임
+            var intent= Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"))
+            startActivity(intent)
         }
     }
 
