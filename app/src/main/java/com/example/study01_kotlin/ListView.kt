7833a44f7afbc1dfba05344445b3e2dabfe28b93
N0mercy00 <dkdhehesp@naver.com>
2021-02-13 16:52:47 +0900

    ListView  Example3 Use ViewHolder

--- app/src/main/java/com/example/study01_kotlin/ListView.kt
+++ app/src/main/java/com/example/study01_kotlin/ListView.kt
@@ -52,14 +52,38 @@ class ListViewAdapter(
     }
     //실제로 가져오는 부분
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view = layoutInflater.inflate(R.layout.item_view,null)
-        var carNameTextView = view.findViewById<TextView>(R.id.tv_carname)
-        var carEngineTextView = view.findViewById<TextView>(R.id.tv_carengine)
 
-        carNameTextView.setText(Car.get(position).name)
-        carEngineTextView.setText(Car.get(position).engine)
+        val view : View
+        val holder : ViewHoler
+
+        //convertView 가 재활용 될 View 없는 경우 만들어주기 (처음엔 하나 만들어줘야지)
+        if(convertView==null){
+            view=layoutInflater.inflate(R.layout.item_view,null)
+            holder= ViewHoler()
+
+            //찾은다음 홀더에 집어 넣는다.
+            holder.carName = view.findViewById(R.id.tv_carname)
+            holder.carEngine= view.findViewById(R.id.tv_carengine)
+
+            //찾을수 있게 태그를 달아둔다다
+            view.tag=holder
+        }else{
+            //재활용할 View가 있는 경우  넣어주기 솔직히 이부분 이해안됨
+                //태그는 뭐고 대충 걍 재활용한다는 것만 알겠음
+            holder=convertView.tag as ViewHoler
+            view =convertView
+        }
+
+        //재활용은 재활용이고 데이터도 넣어줘야함
+        holder.carName?.setText(Car.get(position).name)
+        holder.carEngine?.setText(Car.get(position).engine)
         return view
 
     }
 
+}
+
+class ViewHoler{
+    var  carName : TextView?=null
+    var carEngine : TextView? =null
 }
