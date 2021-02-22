062b5e982d09a270fb037abeb877bb39046247b5
N0mercy00 <dkdhehesp@naver.com>
2021-02-22 19:16:54 +0900

    DONGSTAGRAM still work i dont know whats wrong :) OMG

--- app/src/main/java/com/example/study01_kotlin/MasterApplication.kt
+++ app/src/main/java/com/example/study01_kotlin/MasterApplication.kt
@@ -1,27 +1,35 @@
 package com.example.study01_kotlin
 
 import android.app.Application
+import android.content.Context
 import com.facebook.stetho.okhttp3.StethoInterceptor
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class MasterApplication:Application() {
+class MasterApplication : Application() {
 
-    lateinit var service : RetrofitService
+    lateinit var service: RetrofitService
     override fun onCreate() {
         super.onCreate()
+        createRetrofit()
     }
 
     //나가는 통신 잡아서 헤더 붙혀줌
     fun createRetrofit() {
         val header = Interceptor {
             val original = it.request()
-            val request = original.newBuilder()
-                .header("Authorization", "")
-                .build()
-            it.proceed(request)
+            if (checkIsLogin()) {
+                getUserToken()?.let {token->
+                    val request = original.newBuilder()
+                        .header("Authorization", "token "+token)
+                        .build()
+                    it.proceed(request)
+                }
+            } else {
+                it.proceed(original)
+            }
         }
 
         val client = OkHttpClient.Builder()
@@ -36,13 +44,23 @@ class MasterApplication:Application() {
             .client(client)
             .build()
 
-        service= retrofit.create(RetrofitService::class.java)
+        service = retrofit.create(RetrofitService::class.java)
     }
 
-    fun checkIsLogin():Boolean{
-        //여기서 로그인 되었는지 확인
-        //아직 구현 안한거임
-        return true
+    //로그인이 되었는지 안되었는지 확인해주는 함수
+    fun checkIsLogin(): Boolean {
+        val sp = getSharedPreferences("login_sp", Context.MODE_PRIVATE)
+        val token = sp.getString("login_sp", "null")
+
+        if (token != "null") return true
+        else return false
+    }
+
+    fun getUserToken():String?{
+        val sp = getSharedPreferences("login_sp", Context.MODE_PRIVATE)
+        val token = sp.getString("login_sp", "null")
+        if(token=="null") return null
+        else return token
     }
 
 }
